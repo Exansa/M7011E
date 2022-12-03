dbc0fb36ae51546ecd6be6cdcd5a0a29306b2b3a
TovahParnes <tovah@parnes.com>
2022-12-03 18:24:07 +0100

    cleaned up code, needs tobe tested

--- Backend/tovahtest/db.ts
+++ Backend/tovahtest/db.ts
@@ -14,12 +14,28 @@ export default () => {
 			serverApi: ServerApiVersion.v1
 		});
 
-		const collection = await client.db('test').collection('testcollection');
+		try {
+			var data = req.body.data;
+			const collection = await client
+				.db('test')
+				.collection('testcollection');
 
-		collection.insertOne(req.body);
+			const result = await collection.insertOne(data);
 
+			result
+				? res.status(200).send(`Document created successfully`)
+				: res.status(304).send(`Document not created`);
+			console.log(result);
+		} catch (err) {
+			if (err instanceof Error) {
+				console.error(err.message);
+				res.status(400).send(err.message);
+			} else {
+				console.log('Unexpected error', err);
+				res.status(400).send('Unexpected error');
+			}
+		}
 		client.close();
-		res.send(req.body);
 	});
 
 	router.get('/', async (req, res, next) => {
@@ -31,10 +47,8 @@ export default () => {
 			serverApi: ServerApiVersion.v1
 		});
 
-		var id = req.body._id;
-		var idObject = new ObjectId(id);
-
 		try {
+			var id = req.body._id;
 			const collection = await client
 				.db('test')
 				.collection('testcollection');
@@ -55,43 +69,7 @@ export default () => {
 				res.status(400).send('Unexpected error');
 			}
 		}
-
-		/*
-		const collection = client.db('test').collection('testcollection');
-
-		try {
-			const data = await collection.find({});
-			res.status(200).send(data);
-		} catch (error) {
-			res.status(500).send('error with GET request');
-			//res.status(500).send(error.message);
-		}*/
-
 		client.close();
-		//res.send(req.body);
-
-		/*try {
-			const games = (await collections.games
-				.find({})
-				.toArray()) as Game[];
-
-			res.status(200).send(games);
-		} catch (error) {
-			res.status(500).send(error.message);
-		}
-
-		try {
-			const query = { _id: new ObjectId(id) };
-			const game = (await collections.games.findOne(query)) as Game;
-
-			if (game) {
-				res.status(200).send(game);
-			}
-		} catch (error) {
-			res.status(404).send(
-				`Unable to find matching document with id: ${req.params.id}`
-			);
-		}*/
 	});
 
 	router.patch('/', async (req, res, next) => {
@@ -103,11 +81,9 @@ export default () => {
 			serverApi: ServerApiVersion.v1
 		});
 
-		var id = req.body._id;
-		var data = req.body.data;
-		var idObject = new ObjectId(id);
-
 		try {
+			var id = req.body._id;
+			var data = req.body.data;
 			const collection = await client
 				.db('test')
 				.collection('testcollection');
@@ -143,15 +119,10 @@ export default () => {
 			// useUnifiedTopology: true,
 			serverApi: ServerApiVersion.v1
 		});
-		//const id = req?.params?.id;
-
-		console.log(req.body);
-		var id = req.body._id;
-		var data = req.body.data;
-		var idObject = new ObjectId(id);
-		console.log(idObject);
 
 		try {
+			var id = req.body._id;
+			var data = req.body.data;
 			const collection = await client
 				.db('test')
 				.collection('testcollection');
