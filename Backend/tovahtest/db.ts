b47d15296fa2adcb12ede8ce38d844082495a755
TovahParnes <tovah@parnes.com>
2022-12-03 16:12:34 +0100

    functioning put, need to add wait to take in id

--- Backend/tovahtest/db.ts
+++ Backend/tovahtest/db.ts
@@ -1,5 +1,6 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import { Router, Request, Response } from 'express';
+import { ObjectId } from 'mongodb';
 
 const router = Router();
 
@@ -69,6 +70,55 @@ export default () => {
 				`Unable to find matching document with id: ${req.params.id}`
 			);
 		}*/
+	});
+
+	router.put('/', async (req, res, next) => {
+		const uri =
+			'mongodb+srv://admin:admin@cluster0.jdbug59.mongodb.net/?retryWrites=true&w=majority';
+		const client = new MongoClient(uri, {
+			// useNewUrlParser: true,
+			// useUnifiedTopology: true,
+			serverApi: ServerApiVersion.v1
+		});
+		//const id = req?.params?.id;
+
+		const id = '638b64fe0ee338cecc37b568';
+		console.log(req.body);
+		//var id = req.body._id;
+		var idObject = new ObjectId(id);
+		console.log(idObject);
+
+		try {
+			const collection = await client
+				.db('test')
+				.collection('testcollection');
+			const query = { _id: new ObjectId(id) };
+
+			const result = await collection.updateOne(query, {
+				$set: req.body
+			});
+
+			result
+				? res
+						.status(200)
+						.send(`Successfully updated document with id ${id}`)
+				: res.status(304).send(`Document with id: ${id} not updated`);
+		} catch (err) {
+			if (err instanceof Error) {
+				console.error(err.message);
+				res.status(400).send(err.message);
+			} else {
+				console.log('Unexpected error', err);
+				res.status(400).send('Unexpected error');
+			}
+		}
+
+		const collection = await client.db('test').collection('testcollection');
+
+		collection.insertOne(req.body);
+
+		client.close();
+		res.send(req.body);
 	});
 
 	return router;
