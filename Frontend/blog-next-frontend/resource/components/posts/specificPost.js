f3327fcb781a673fd2802a603a698e4ad966b390
Elliot <ellioth70@gmail.com>
2022-12-06 10:37:57 +0100

    Image pekar rätt

--- Frontend/blog-next-frontend/resource/components/posts/specificPost.js
+++ Frontend/blog-next-frontend/resource/components/posts/specificPost.js
@@ -4,12 +4,12 @@ export default function SpecificPost({ item }) {
   return (
     <Box>
       <Stack direction="column" spacing={2} sx={{ mb: 2 }}>
-        <Image src={item.image} alt={item.title} />
+        <Image src={item.image.href} alt={item.title} />
         <Typography variant="h1" component="h2">
           {item.title}
         </Typography>
         <Stack direction="row" spacing={2} sx={{ mb: 2 }}>
-          <Avatar src={item.user.image} alt={item.user.name} />
+          <Avatar src={item.user.image.href} alt={item.user.name} />
           <Typography variant="h4" component="h3">
             {item.user.name}
           </Typography>
