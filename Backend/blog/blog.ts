8ad6fa408c8fb77e12228fa0da0889e0cc16a2eb
TovahParnes <tovah@parnes.com>
2022-12-06 11:45:26 +0100

    added get for a users all posts, need to find a way ti iterate through them

--- Backend/blog/blog.ts
+++ Backend/blog/blog.ts
@@ -36,7 +36,7 @@ export default () => {
 		client.close();
 	});
 
-	router.get('/', async (req, res, next) => {
+	router.get('/:user', async (req, res, next) => {
 		const uri =
 			'mongodb+srv://admin:admin@cluster0.jdbug59.mongodb.net/?retryWrites=true&w=majority';
 		const client = new MongoClient(uri, {
@@ -45,17 +45,54 @@ export default () => {
 			serverApi: ServerApiVersion.v1
 		});
 
+		const userId = req?.params?.user;
+
+		/*try {
+			var postId = req.body._id;
+			if (!postId) {
+				throw new Error('No post id provided');
+			}
+		} catch (err) {
+			if (err instanceof Error) {
+				console.error(err.message);
+				res.status(400).send(err.message);
+			} else {
+				console.log('Unexpected error', err);
+				res.status(400).send('Unexpected error');
+			}
+		}*/
+
 		try {
-			var id = req.body._id;
 			const collection = await client.db('blog').collection('posts');
-			const query = { _id: new ObjectId(id) };
+			const query = { user_id: userId };
 
-			const result = await collection.findOne(query);
+			var myCursor = await collection.findOne(query);
+			console.log('my  cursor: ' + myCursor);
 
+			//TODO: find a way to iterate through the cursor
+
+			/*
+            var runCursor = function (cursor : MongoCursor){
+                cursor.next(function(error : any, record : any){
+
+			if (myCursor != null) {
+				var myDocument = myCursor.next();
+				console.log('my document: ' + myDocument);
+				if (myDocument) {
+					var title = myDocument.title;
+					console.log('title: ' + title);
+				}
+			} else {
+				console.log('No document found');
+			}*/
+
+			const result = null;
 			result
 				? res.status(200).send(result)
-				: res.status(304).send(`Document with id: ${id} not retrieved`);
-			console.log(result);
+				: res
+						.status(304)
+						.send(`Blogposts form user ${userId} not retrieved`);
+			//console.log(result);
 		} catch (err) {
 			if (err instanceof Error) {
 				console.error(err.message);
