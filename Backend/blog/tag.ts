4d6ae7e477ef41b95a81c90578dcd9ac9243e130
TovahParnes <tovah@parnes.com>
2022-12-07 21:26:40 +0100

    added patch

--- Backend/blog/tag.ts
+++ Backend/blog/tag.ts
@@ -19,6 +19,7 @@ export default () => {
 		try {
 			checkCurrentUser(userId, req.body.user_id);
 			var tag = req.body.tag;
+			tag = generateTag(tag, userId);
 			validateTag(tag);
 			console.log(tag);
 
@@ -41,6 +42,49 @@ export default () => {
 		}
 		client.close();
 	});
+
+	router.patch('/:user/:tag', async (req, res, next) => {
+		const uri =
+			'mongodb+srv://admin:admin@cluster0.jdbug59.mongodb.net/?retryWrites=true&w=majority';
+		const client = new MongoClient(uri, {
+			// useNewUrlParser: true,
+			// useUnifiedTopology: true,
+			serverApi: ServerApiVersion.v1
+		});
+
+		const userId = req?.params?.user;
+		const tagId = req?.params?.tag;
+
+		try {
+			checkCurrentUser(userId, req.body.user_id);
+			var tag = req.body.tag;
+			validateTag(tag);
+			console.log(tag);
+
+			const collection = await client.db('blog').collection('tags');
+			const query = { _id: new ObjectId(tagId), user_id: userId };
+
+			const result = await collection.updateOne(query, {
+				$set: tag
+			});
+
+			result
+				? res
+						.status(200)
+						.send(`Tag with id ${tagId} updated successfully`)
+				: res.status(304).send(`Tag with id ${tagId} not updated`);
+			console.log(result);
+		} catch (err) {
+			if (err instanceof Error) {
+				console.error(err.message);
+				res.status(400).send(err.message);
+			} else {
+				console.log('Unexpected error', err);
+				res.status(400).send('Unexpected error');
+			}
+		}
+		client.close();
+	});
 	return router;
 };
 
@@ -50,4 +94,10 @@ function checkCurrentUser(userId: string, userId1: any) {
 
 function validateTag(tag: any) {
 	if (!tag.name) throw new Error('Name is not defined, invalid tag');
+}
+function generateTag(tag: any, userId: string): any {
+	return {
+		user_id: userId,
+		name: tag.name
+	};
 }
