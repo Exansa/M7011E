671502e168fb7f4a4ee7d823c1fdf73c7ef86539
Elliot <ellioth70@gmail.com>
2022-12-07 16:26:45 +0100

    User-related pages initiated

--- Frontend/blog-next-frontend/pages/profile/index.js
+++ Frontend/blog-next-frontend/pages/profile/index.js
@@ -1,11 +1,23 @@
 import ResponsiveAppBar from "../../resource/components/global/headerBar";
+import { Box, Stack, Avatar } from "@mui/material";
+import Users from "../../data/mock_db/users";
+import Page from "../../resource/components/page";
 
 export function Profile() {
-    return (
-        <Box>
-          <ResponsiveAppBar />
-        </Box>
+  let user = Users[0]; //TODO: Auth
 
-    );
+  return (
+    <>
+      <Page title={"Profile: " + user.username}>
+        <ResponsiveAppBar />
+
+        <Box sx={{ mx: 4, my: 4 }}>
+          <Stack direction="column">
+            <Avatar alt="Error loading avatar" src={user.image.href}></Avatar>
+          </Stack>
+        </Box>
+      </Page>
+    </>
+  );
 }
 export default Profile;
