2bddc24810ab34c0920038415e5966eb6a24ece2
TovahParnes <tovah@parnes.com>
2022-12-09 12:47:52 +0100

    added admin access to categories

--- Backend/blog/category.ts
+++ Backend/blog/category.ts
@@ -15,19 +15,16 @@ export default () => {
 		});
 
 		try {
+			var userId = req.body.user_id;
 			var category = req.body.category;
 			category = generateCategory(category);
 			validateCategory(category);
 
+			await checkAccess(userId, client);
+
+			await checkCategoryExists(category, client);
+
 			const collection = await client.db('blog').collection('categories');
-
-			const query1 = {
-				name: category.name
-			};
-			const alreadyExists = await collection.findOne(query1);
-			console.log(alreadyExists);
-			if (alreadyExists) throw new Error('Category already exists');
-
 			const result = await collection.insertOne(category);
 
 			result
@@ -58,25 +55,22 @@ export default () => {
 		const categoryId = req?.params?.category;
 
 		try {
+			var userId = req.body.user_id;
+
 			var category = req.body.category;
 			validateCategory(category);
 
+			await checkAccess(userId, client);
+
+			await checkUniuqeCategory(category, categoryId, client);
+
 			const collection = await client.db('blog').collection('categories');
-
-			const query1 = {
-				name: category.name,
-				_id: { $ne: new ObjectId(categoryId) }
-			};
-			const alreadyExists = await collection.findOne(query1);
-			console.log(alreadyExists);
-			if (alreadyExists) throw new Error('Category already exists');
-
 			const query = { _id: new ObjectId(categoryId) };
 			const result = await collection.updateOne(query, {
 				$set: category
 			});
 
-			result
+			result && result.modifiedCount
 				? res
 						.status(200)
 						.send(
@@ -111,4 +105,35 @@ function validateCategory(category: any) {
 		throw new Error('Name is not defined, invalid category');
 	if (!category.description)
 		throw new Error('Description is not defined, invalid category');
+}
+
+async function checkAccess(userId: any, client: MongoClient) {
+	const collection = await client.db('blog').collection('admins');
+	const query = { user_id: userId };
+	const result = await collection.findOne(query);
+	if (!result || result.access != 'superAdmin')
+		throw new Error('Access denied');
+}
+async function checkCategoryExists(category: any, client: MongoClient) {
+	const collection = await client.db('blog').collection('categories');
+	const query1 = {
+		name: category.name
+	};
+	const alreadyExists = await collection.findOne(query1);
+	if (alreadyExists) throw new Error('Category already exists');
+}
+
+async function checkUniuqeCategory(
+	category: any,
+	categoryId: string,
+	client: MongoClient
+) {
+	const collection = await client.db('blog').collection('tags');
+	const query1 = {
+		name: category.name,
+		_id: { $ne: new ObjectId(categoryId) }
+	};
+	const alreadyExists = await collection.findOne(query1);
+	if (alreadyExists)
+		throw new Error('Category with that name already exists');
 }
