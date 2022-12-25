4565fb7114d4acdab7a5f8f57569e95f4f11c75a
Elliot <ellioth70@gmail.com>
2022-12-25 13:28:18 +0100

    Simple specific post page, alignment is a TODO

--- Frontend/blog-flutter-frontend/my_app/lib/pages/posts/post.dart
+++ Frontend/blog-flutter-frontend/my_app/lib/pages/posts/post.dart
@@ -18,6 +18,47 @@ class SpecificPost extends State<Post> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [GenericPostCard(post: post)]));
+    final theme = Theme.of(context);
+
+    return Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 1200,
+          height: 630,
+          child: FittedBox(
+              clipBehavior: Clip.hardEdge,
+              child: Image.network(
+                post["image"]["href"],
+                fit: BoxFit.fitWidth,
+              )),
+        ),
+        Row(children: [
+          CircleAvatar(
+              backgroundImage: NetworkImage(post["user"]["image"]["href"])),
+          Column(children: [
+            Text(post["title"], style: theme.textTheme.displaySmall),
+            Text("by ${post["user"]["username"]}",
+                style: theme.textTheme.subtitle1)
+          ])
+        ]),
+        RichText(
+            overflow: TextOverflow.ellipsis,
+            text: TextSpan(
+              children: [
+                TextSpan(
+                    //Subject to change :)
+                    text: post["category"]["name"],
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.grey[800])),
+                // ignore: prefer_interpolation_to_compose_strings
+                TextSpan(
+                    text: " - ${post["content"]}",
+                    style: theme.textTheme.bodyMedium)
+              ],
+            ))
+      ],
+    ));
   }
 }
