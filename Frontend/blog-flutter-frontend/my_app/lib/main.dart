e8320bc7c7372a7026194739589d1454dccacaa5
Elliot <ellioth70@gmail.com>
2022-12-25 16:13:59 +0100

    Main and nav upgraded somewhat

--- Frontend/blog-flutter-frontend/my_app/lib/main.dart
+++ Frontend/blog-flutter-frontend/my_app/lib/main.dart
@@ -26,7 +26,21 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/home',
       getPages: appRoutes(),
-      //home: Home(),
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
--- Frontend/blog-flutter-frontend/my_app/lib/widgets/navigationBar.dart
+++ Frontend/blog-flutter-frontend/my_app/lib/widgets/navigationBar.dart
@@ -7,17 +7,50 @@ class NavigationBarWeb extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100.0,
-      color: Get.theme.primaryColor,
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: const [
-          NavigationItem(title: 'Home', route: '/home'),
-          NavigationItem(title: 'About', route: '/about'),
-          NavigationItem(title: 'Browse', route: '/browse'),
-        ],
-      ),
-    );
+    var screenSize = MediaQuery.of(context).size;
+
+    return PreferredSize(
+        preferredSize: Size(screenSize.width, 1000),
+        child: Container(
+            height: 100.0,
+            color: Get.theme.primaryColor,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const Text("M7011E: Blog"),
+                  Expanded(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const NavigationItem(title: 'Home', route: '/home'),
+                          SizedBox(width: screenSize.width / 30),
+                          const NavigationItem(title: 'About', route: '/about'),
+                          SizedBox(width: screenSize.width / 30),
+                          const NavigationItem(
+                              title: 'Browse', route: '/browse'),
+                        ]),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    width: screenSize.width / 50,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            )));
   }
 }
