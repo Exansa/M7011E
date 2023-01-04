5c09b477a1b9b1e4a7cb48aceb5a6f56c100307c
Elliot <ellioth70@gmail.com>
2023-01-04 14:21:33 +0100

    Auth0 impl, token not grabbed

--- Frontend/blog-flutter-frontend/my_app/lib/main.dart
+++ Frontend/blog-flutter-frontend/my_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auth0_flutter2/auth0_flutter2.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_app/appView.dart';
@@ -5,6 +6,15 @@ import 'package:my_app/pages/home.dart';
 import 'package:my_app/routing/routes.dart';
 
 void main() {
+  Auth0Flutter2.auth0Domain = "peterpanduro.eu.auth0.com";
+  Auth0Flutter2.auth0ClientId = "nNei1uMJNluLsmy6BWvo4eTaaTHnczB9";
+  Auth0Flutter2.redirectUri = "http://localhost:3000";
+
+  // Set the URL strategy for our web app. Removes
+  // trailing hash(#) to ensure login callbacks
+  // will be captured and processed correctly.
+  Auth0Flutter2.setPathUrlStrategy();
+
   runApp(MyApp());
 }
 
@@ -40,7 +50,26 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    checkForRedirectCallback();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  Future<void> checkForRedirectCallback() async {
+    // Check for login callback state.
+    var redirectCallbackHandled =
+        await Auth0Flutter2.instance.handleWebLoginCallback(
+      Uri.base.toString(),
+    );
+
+    // If callback was able to be processed, do something.
+    if (redirectCallbackHandled) {
+      // DO SOMETHING...
+    }
   }
 }
--- Frontend/blog-flutter-frontend/my_app/lib/widgets/navigationItem.dart
+++ Frontend/blog-flutter-frontend/my_app/lib/widgets/navigationItem.dart
@@ -1,3 +1,4 @@
+import 'package:auth0_flutter2/auth0_flutter2.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_app/pages/home.dart';
@@ -32,15 +33,15 @@ class NavigationItemState extends State<NavigationItem> {
         hovering = value;
       }),
       onTap: () {
-        Get.toNamed(route);
-        // if (route != "/login" && route != "/logout") {
-        //   Get.toNamed(route);
-        // } else if (route == "/login") {
-        //   print("Login pressed");
-        //   GoogleSignInApi.Login();
-        // } else {
-        //   GoogleSignInApi.SignOut();
-        // }
+        //Get.toNamed(route);
+        if (route != "/login" && route != "/logout") {
+          Get.toNamed(route);
+        } else if (route == "/login") {
+          print("Login pressed");
+          Auth0Flutter2.instance.loginUser().then((value) => print(value));
+        } else {
+          Auth0Flutter2.instance.logoutUser();
+        }
       },
       child: Text(
         title,
