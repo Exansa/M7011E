60c4ef6b89f37144240aad6b6c1cbcf2d3c76a2e
Elliot <ellioth70@gmail.com>
2023-01-05 14:32:07 +0100

    Cleanup + Added a video to the about page

--- Frontend/blog-next-frontend/next.config.js
+++ Frontend/blog-next-frontend/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+//Next-videos
+// const withVideos = require("next-videos");
+
+// module.exports = withVideos({
+//   assetPrefix:
+//     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/",
+
+//   webpack(config, options) {
+//     return config;
+//   },
+// });
