56d6c75e08215eeb88c31cc7ea7bbd857ec858ea
stenis <magset-8@student.ltu.se>
2023-01-10 16:53:00 +0100

    edit post works but it does not reset the text boxes

--- Frontend/blog-next-frontend/pages/posts/edit/[postID].js
+++ Frontend/blog-next-frontend/pages/posts/edit/[postID].js
@@ -112,7 +112,7 @@ const editPost = ( context ) => {
     const JSONdata = JSON.stringify(data)
     console.log(JSONdata)
     console.log(context.postID)
-    const postRef = "http://localhost:5001/post/" + context.postID;
+    const postRef = "http://localhost:5001/posts/" + context.postID;
     const res = await fetch(postRef, {
         method: "PATCH",
         headers: {
