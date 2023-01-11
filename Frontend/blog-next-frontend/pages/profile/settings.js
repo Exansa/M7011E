fc6dbba0b90c9015319ee741ca768966e7e404f7
stenis <magset-8@student.ltu.se>
2023-01-11 16:19:03 +0100

    Edit user username and email added

--- Frontend/blog-next-frontend/pages/profile/settings.js
+++ Frontend/blog-next-frontend/pages/profile/settings.js
@@ -5,6 +5,7 @@ import Page from "../../resource/layout/page";
 import Users from "../../data/mock_db/users";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 import { NextApiRequest, NextApiResponse } from "next";
 import AccessDenied from "../../resource/components/accessDenied";
 
@@ -40,28 +41,95 @@ export function Settings() {
 
   const handleSubmit = async (event)=> {
     event.preventDefault()
+
+    console.log('submit')
     if(event.target.username.value=="" && event.target.email.value==""){
-      const data = {
+      const preData = {
         username: session.user.username,
         email: session.user.email,
         profile_picture: session.user.profile_picture
       }
+      const data = {
+        user: preData
+      }
+      const path = 'http://localhost:5001/user/' + session.user._id;
+      const access = "Bearer " + session.accessToken;
+      const res = await fetch(path, {
+      method: "PATCH",
+      headers: {
+        accept: "*/*", 
+        Authorization: access,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data),
+      });
     }
     else if(event.target.username.value==""){
-      const data = {
+      const preData = {
         username: session.user.username,
         email: event.target.email.value,
         profile_picture: session.user.profile_picture
       }
+      const data = {
+        user: preData
+      }
+      const path = 'http://localhost:5001/user/' + session.user._id;
+      const access = "Bearer " + session.accessToken;
+      const res = await fetch(path, {
+      method: "PATCH",
+      headers: {
+        accept: "*/*", 
+        Authorization: access,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data),
+      });
     }
     else if(event.target.email.value==""){
-      const data = {
+      console.log('hej')
+      const preData = {
         username: event.target.username.value,
         email: session.user.email,
         profile_picture: session.user.profile_picture
       }
+      const data = {
+        user: preData
+      }
+      const path = 'http://localhost:5001/user/' + session.user._id;
+      const access = "Bearer " + session.accessToken;
+      const res = await fetch(path, {
+      method: "PATCH",
+      headers: {
+        accept: "*/*", 
+        Authorization: access,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data),
+      });
+      const resp = await res.json()
+    } else{
+      const preData = {
+        username: event.target.username.value,
+        email: event.target.email.value,
+        profile_picture: session.user.profile_picture
+      }
+      const data = {
+        user: preData
+      }
+      const path = 'http://localhost:5001/user/' + session.user._id;
+      const access = "Bearer " + session.accessToken;
+      const res = await fetch(path, {
+      method: "PATCH",
+      headers: {
+        accept: "*/*", 
+        Authorization: access,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data),
+      });
     }
-    console.log(data)
+
+    
     /*
     const path = 'http://localhost:5001/user/' + session.user._id;
     const access = "Bearer " + session.accessToken;
@@ -84,27 +152,31 @@ export function Settings() {
       <Page title={"Settings: " + session.name}>
        <Container maxWidth="md">
        <form onSubmit={handleSubmit}>
-          <TextField  required fullWidth label="New username" 
+          <TextField  
+                      fullWidth 
+                      label="New username" 
                       margin="normal" 
                       id='username'
                       name="username"
-                      onChange={(newValue) => {
+                      /*onChange={(newValue) => {
                         setValue(newValue.target.value);
                       }}
                       variant="outlined"
                       defaultValue={value}
-                      value={value}
+                      value={value}*/
                       />
-          <TextField  required fullWidth label="New email" 
+          <TextField  
+                      fullWidth 
+                      label="New email" 
                       margin="normal" 
                       id='email'
                       name="email"
-                      onChange={(newValue) => {
+                      /*onChange={(newValue) => {
                         setValue(newValue.target.value);
                       }}
                       variant="outlined"
                       defaultValue={value}
-                      value={value}
+                      value={value}*/
                       />
           <Button
               sx={{ m: 1, minWidth: 120 }}
