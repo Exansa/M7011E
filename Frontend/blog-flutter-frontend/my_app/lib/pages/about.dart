40241c6617a3bc545048357b738c50e66f9554ee
stenis <magset-8@student.ltu.se>
2023-01-12 23:14:25 +0100

    Flutter version now has a copycat about page

--- Frontend/blog-flutter-frontend/my_app/lib/pages/about.dart
+++ Frontend/blog-flutter-frontend/my_app/lib/pages/about.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_app/widgets/videoPlayer.dart';
 
 class About extends StatelessWidget {
   const About({Key key}) : super(key: key);
@@ -10,11 +11,32 @@ class About extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ElevatedButton(
-              onPressed: () {
-                Get.back();
-              },
-              child: const Text("Open other screen"))
+          VideoPlayerScreen(key),
+          Text(
+            "About this site",
+            style: Theme.of(context).textTheme.headline3, // like <h1> in HTML
+          ),
+          RichText(
+            text: TextSpan(
+              text:
+                  """            The project for M7011E 2022 by the group gamla och bittra; with
+            Elliot Huber, Magnus Stenfelt, Peter Panduro and Tovah Parnes. Our
+            project is a blog style website where we will make it using the
+            frameworks NextJS and Dart and then compare the performance of the
+            two different systems. They will have a shared backend and database
+            so we can see the difference in them without other factors.""",
+              style: DefaultTextStyle.of(context).style,
+              children: const <TextSpan>[
+                TextSpan(text: '\n\n'),
+                TextSpan(
+                    text:
+                        """            You can post things and try the user functionality. If you ever
+            become bored we have placed a nice little video here for you to
+            watch. Enjoy!""",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+          )
         ],
       ),
     );
---
+++ Frontend/blog-flutter-frontend/my_app/lib/widgets/videoPlayer.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen(Key key) : super(key: key);
+
+  @override
+  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.network(
+      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    );
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    _controller.setLooping(true);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // mutes the video
+      _controller.setVolume(0);
+      // Plays the video once the widget is build and loaded.
+      _controller.play();
+    });
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Complete the code in the next step.
+    return Container(
+      height: 450,
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: _initializeVideoPlayerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If the VideoPlayerController has finished initialization, use
+              // the data it provides to limit the aspect ratio of the video.
+              return AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                // Use the VideoPlayer widget to display the video.
+                child: VideoPlayer(_controller),
+              );
+            } else {
+              // If the VideoPlayerController is still initializing, show a
+              // loading spinner.
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
