67e02cb3266c97b13d434ea4a4763c74f519fb82
Elliot <ellioth70@gmail.com>
2023-01-13 19:45:21 +0100

    Authorization now uses the server

--- Frontend/blog-next-frontend/pages/_app.js
+++ Frontend/blog-next-frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { SessionProvider, useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
 import Page from "../resource/layout/page";
 import Loading from "../resource/layout/loading";
 import AccessDenied from "../resource/components/accessDenied";
@@ -23,15 +24,33 @@ function Auth(props) {
   // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
   const { status, data } = useSession({ required: true });
 
+  const [admin, setAdmin] = useState(false);
+
+  // This is called whenever the session changes or a page with auth is loaded
+  useEffect(() => {
+    const fetchAdmin = async () => {
+      const res = await fetch("http://localhost:5001/admins/me", {
+        method: "GET",
+        headers: {
+          authorization: "Bearer " + data.accessToken,
+        },
+      });
+      if (res.status === 200) {
+        setAdmin(true);
+      }
+    };
+
+    if (status !== "loading" && auth.admin) {
+      fetchAdmin();
+    }
+  }, [auth.admin, data, status]);
+
+  // We want to avoid flashing sensitive content on the screen
   if (!data && status === "loading") {
     return <Loading />;
   }
 
-  if (
-    auth.admin &&
-    data.user.access !== "admin" &&
-    data.user.access !== "superAdmin"
-  ) {
+  if (auth.admin && !admin) {
     return (
       <Page title="Access denied">
         <AccessDenied />
