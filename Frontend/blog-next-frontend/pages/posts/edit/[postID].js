e7b23a48eaf6c9c02bba76aa8b6786a267e391d0
stenis <magset-8@student.ltu.se>
2023-01-13 01:47:17 +0100

    Fixed editpost

--- Frontend/blog-next-frontend/pages/posts/edit/[postID].js
+++ Frontend/blog-next-frontend/pages/posts/edit/[postID].js
@@ -39,6 +39,8 @@ export async function getStaticProps({ params }) {
   const tags = await resTags.json();
   const resCat = await fetch("http://localhost:5001/categories?set=1");
   const categories = await resCat.json();
+  const resPost = await fetch("http://localhost:5001/posts/1");
+  const post = await resPost.json();
   //console.log('2')
   //console.log('params',params.postID);
   //console.log(tags);
@@ -48,6 +50,7 @@ export async function getStaticProps({ params }) {
     props: {
       tags,
       categories,
+      post,
       postID: params.postID,
     },
   };
@@ -108,7 +111,7 @@ export default function EditPost(context) {
   if (
     !session ||
     (session &&
-      session.user._id != context.post.user_id &&
+      session.user._id !== context.post.user_id &&
       session.user.access !== "admin" &&
       session.user.access !== "superAdmin")
   ) {
@@ -156,7 +159,7 @@ export default function EditPost(context) {
               >
                 {context.categories.map((categories) => (
                   <MenuItem
-                    key={`menu-item-${categories._id}`}
+                    key={`menu-item-${context.categories._id}`}
                     value={categories._id}
                   >
                     {categories.name}
@@ -176,7 +179,7 @@ export default function EditPost(context) {
               >
                 {context.tags.map((tags) => (
                   <MenuItem
-                    key={`menu-item-${categories._id}`}
+                    key={`menu-item-${context.tags._id}`}
                     value={tags._id}
                   >
                     {tags.name}
